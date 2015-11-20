963eb0b61a2f3c0fd90b19dfdec6c3a079295f0a
K106 <m016248e@k106-12.student.staffs.ac.uk>
2015-11-20 11:30:36 +0000

    Prepared header file for basic game elements

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -2,6 +2,9 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+#include "ui/CocosGUI.h"
+
+class Credits;
 
 class HelloWorld : public cocos2d::Layer
 {
@@ -14,6 +17,28 @@ public:
 
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+
+	void Update(float);
+
+	virtual bool onTouchBegan(cocos2d::Touch*, cocos2d::Event*);
+	virtual void onTouchEnded(cocos2d::Touch*, cocos2d::Event*);
+	virtual void onTouchMoved(cocos2d::Touch*, cocos2d::Event*);
+	virtual void onTouchCancelled(cocos2d::Touch*, cocos2d::Event*);
+
+	void StartButtonPressed(Ref *sender, cocos2d::ui::Widget::TouchEventType type);
+	void CreditsButtonPressed(Ref *sender, cocos2d::ui::Widget::TouchEventType type);
+
+	void StartGame();
+	void EndGame();
+
+private:
+	cocos2d::Sprite* Paddle;
+	cocos2d::Sprite* Ball;
+	cocos2d::Sprite* Block;
+	cocos2d::Label* scoreLabel;
+	cocos2d::Label* ammoCount;
+	cocos2d::Label* livesCount;
+	cocos2d::Label* currentPower;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
