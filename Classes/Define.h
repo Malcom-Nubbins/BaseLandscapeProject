6dbcbc2720328232b8e76bab8055ad4ea01857e2
James E <e018719c@student.staffs.ac.uk>
2015-12-08 15:20:14 +0000

    Started spawning bricks and power ups relative to screen size, rather than vec2 positions.  In the lack of using txt or p.lists or having a very big class full of levels, have made a define for levels only, very messy enter with caution.

--- Classes/Define.h
+++ Classes/Define.h
@@ -3,7 +3,7 @@
 
 // Spash Screen Specific
 
-#define Splash_Length 3.0 // Defines the length of splash screen.
+#define Splash_Length 0.5 // Defines the length of splash screen. Temporarily reduced splash screen time for ease of debugging
 #define Transition_Length 0.8 // Defines the length of transitions.
 #define Load_MaxTime 8 // Used to prevent infinte loading.
 
@@ -27,44 +27,13 @@
 #define Brick_Debug 0.00005
 #define Brick_Distance 0
 #define Brick_Line_Distance 0
-#define Brick_Size 60
+#define Brick_W 60
+#define Brick_H 24
+#define PerLine_Base 16
+#define Lines_Base 6
 #define Left_Max 0
 #define Right_Max 0
 #define Down_Max 0
 #define Up_Max 0
-
-
-
-//Level One		//May be best refracted if time. 
-#define L1_Brick_NO 96
-#define L1_Brick_Violet 64 //must add up to L1_Brick_NO
-#define L1_Brick_Yellow 24 //must add up to L1_Brick_NO
-#define L1_Brick_Orange 8 //must add up to L1_Brick_NO
-#define L1_Brick_Lines 6 //Currently tied to L1_Brick_NO
-#define L1_Brick_Pow 4 //Now Not tied To bricks
-#define L1_Brick_Miss_1 1 
-#define L1_Brick_Miss_2 2 
-#define L1_Brick_Miss_4 4 
-#define L1_Brick_Miss_8 8 
-//Note to self add Colour blocks etc
-
-//Level Two
-#define L2_Brick_NO 64
-#define L2_Brick_Lines 6
-#define L2_Brick_Pow 4
-
-//Level Three
-#define L3_Brick_NO 64
-#define L3_Brick_Lines 6
-#define L3_Brick_Pow 4
-
-//Level Four
-#define L4_Brick_NO 64
-#define L4_Brick_Lines 6
-#define L4_Brick_Pow 4
-
-//Level Five
-#define L5_Brick_NO 64
-#define L5_Brick_Lines 6
-#define L5_Brick_Pow 4
-#endif // __GAMEMAIN_SCENE_H__
+#define Test 50000,68000
+#endif // __DEFINE_H__
---
+++ proj.win32/LevelDefine.h
@@ -0,0 +1,246 @@
+#ifndef __LEVELDEFINE_H__
+#define __LEVELDEFINE_H__
+
+//our txt file... sorta
+
+//Basic_Line
+#define Basic_Line_Number 16
+#define Basic_Line_1 1,1,0
+#define Basic_Line_2 1,1,1
+#define Basic_Line_3 1,1,0
+#define Basic_Line_4 1,1,0
+#define Basic_Line_5 1,1,0
+#define Basic_Line_6 1,1,0
+#define Basic_Line_7 1,1,0
+#define Basic_Line_8 1,1,0
+#define Basic_Line_9 1,1,0
+#define Basic_Line_10 1,1,0
+#define Basic_Line_11 1,1,0
+#define Basic_Line_12 1,1,0
+#define Basic_Line_13 1,1,0
+#define Basic_Line_14 1,1,0
+#define Basic_Line_15 1,1,1
+#define Basic_Line_16 1,1,0
+
+//Impass_Line
+#define Impass_Line_Number 16
+#define Impass_Line_1 1,0,0
+#define Impass_Line_2 1,0,0
+#define Impass_Line_3 1,0,0
+#define Impass_Line_4 1,0,0
+#define Impass_Line_5 1,0,0
+#define Impass_Line_6 1,0,0
+#define Impass_Line_7 1,0,0
+#define Impass_Line_8 1,0,0
+#define Impass_Line_9 1,0,0
+#define Impass_Line_10 1,0,0
+#define Impass_Line_11 1,0,0
+#define Impass_Line_12 1,0,0
+#define Impass_Line_13 1,0,0
+#define Impass_Line_14 1,0,0
+#define Impass_Line_15 1,0,0
+#define Impass_Line_16 1,0,0
+
+//ImpassMissingNo_Line
+#define ImpassMissingNo_Line_Number 16
+#define ImpassMissingNo_Line_1 1,0,0
+#define ImpassMissingNo_Line_2 0,0,0
+#define ImpassMissingNo_Line_3 0,0,0
+#define ImpassMissingNo_Line_4 0,0,0
+#define ImpassMissingNo_Line_5 0,0,0
+#define ImpassMissingNo_Line_6 0,0,0
+#define ImpassMissingNo_Line_7 0,0,0
+#define ImpassMissingNo_Line_8 0,0,0
+#define ImpassMissingNo_Line_9 0,0,0
+#define ImpassMissingNo_Line_10 0,0,0
+#define ImpassMissingNo_Line_11 0,0,0
+#define ImpassMissingNo_Line_12 0,0,0
+#define ImpassMissingNo_Line_13 0,0,0
+#define ImpassMissingNo_Line_14 0,0,0
+#define ImpassMissingNo_Line_15 0,0,0
+#define ImpassMissingNo_Line_16 0,0,0
+
+//Centered_Line
+#define Centered_Line_Number 16
+#define Centered_Line_1 0,1,0
+#define Centered_Line_2 0,1,0
+#define Centered_Line_3 1,1,0
+#define Centered_Line_4 1,1,0
+#define Centered_Line_5 1,1,0
+#define Centered_Line_6 1,1,0
+#define Centered_Line_7 1,1,0
+#define Centered_Line_8 1,1,1
+#define Centered_Line_9 1,1,1
+#define Centered_Line_10 1,1,0
+#define Centered_Line_11 1,1,0
+#define Centered_Line_12 1,1,0
+#define Centered_Line_13 1,1,0
+#define Centered_Line_14 1,1,0
+#define Centered_Line_15 0,1,1
+#define Centered_Line_16 0,1,0
+
+//MissingNo_Line
+#define MissingNo_Line_Number 16
+#define MissingNo_Line_1 1,1,0
+#define MissingNo_Line_2 0,1,1
+#define MissingNo_Line_3 0,1,0
+#define MissingNo_Line_4 0,1,0
+#define MissingNo_Line_5 0,1,0
+#define MissingNo_Line_6 0,1,0
+#define MissingNo_Line_7 0,1,0
+#define MissingNo_Line_8 0,1,0
+#define MissingNo_Line_9 0,1,0
+#define MissingNo_Line_10 0,1,0
+#define MissingNo_Line_11 0,1,0
+#define MissingNo_Line_12 0,1,0
+#define MissingNo_Line_13 0,1,0
+#define MissingNo_Line_14 0,1,0
+#define MissingNo_Line_15 0,1,1
+#define MissingNo_Line_16 1,1,0
+
+//Box_Line
+#define Box_Line_Number 16
+#define Box_Line_1 0,1,0
+#define Box_Line_2 0,1,0
+#define Box_Line_3 0,1,0
+#define Box_Line_4 0,1,0
+#define Box_Line_5 0,1,0
+#define Box_Line_6 0,1,0
+#define Box_Line_7 0,1,0
+#define Box_Line_8 1,1,0
+#define Box_Line_9 1,1,0
+#define Box_Line_10 0,1,0
+#define Box_Line_11 0,1,0
+#define Box_Line_12 0,1,0
+#define Box_Line_13 0,1,0
+#define Box_Line_14 0,1,0
+#define Box_Line_15 0,1,0
+#define Box_Line_16 0,1,0
+
+//dotted_Line
+#define dotted_Line_Number 16
+#define dotted_Line_1 0,1,0
+#define dotted_Line_2 1,1,1
+#define dotted_Line_3 0,1,0
+#define dotted_Line_4 1,1,0
+#define dotted_Line_5 0,1,0
+#define dotted_Line_6 1,1,0
+#define dotted_Line_7 0,1,0
+#define dotted_Line_8 1,1,0
+#define dotted_Line_9 0,1,0
+#define dotted_Line_10 1,1,0
+#define dotted_Line_11 0,1,0
+#define dotted_Line_12 1,1,0
+#define dotted_Line_13 0,1,0
+#define dotted_Line_14 1,1,0
+#define dotted_Line_15 0,1,1
+#define dotted_Line_16 1,1,0
+
+//dottedR_Line
+#define dottedR_Line_Number 16
+#define dottedR_Line_1 1,1,0
+#define dottedR_Line_2 1,1,1
+#define dottedR_Line_3 1,1,0
+#define dottedR_Line_4 1,1,0
+#define dottedR_Line_5 1,1,0
+#define dottedR_Line_6 1,1,0
+#define dottedR_Line_7 1,1,0
+#define dottedR_Line_8 1,1,0
+#define dottedR_Line_9 1,1,0
+#define dottedR_Line_10 1,1,0
+#define dottedR_Line_11 1,1,0
+#define dottedR_Line_12 1,1,0
+#define dottedR_Line_13 1,1,0
+#define dottedR_Line_14 1,1,0
+#define dottedR_Line_15 1,1,1
+#define dottedR_Line_16 1,1,0
+
+//Basic Line
+#define Basic_Line_Number 16
+#define Basic_Line_1 1,1,0
+#define Basic_Line_2 1,1,1
+#define Basic_Line_3 1,1,0
+#define Basic_Line_4 1,1,0
+#define Basic_Line_5 1,1,0
+#define Basic_Line_6 1,1,0
+#define Basic_Line_7 1,1,0
+#define Basic_Line_8 1,1,0
+#define Basic_Line_9 1,1,0
+#define Basic_Line_10 1,1,0
+#define Basic_Line_11 1,1,0
+#define Basic_Line_12 1,1,0
+#define Basic_Line_13 1,1,0
+#define Basic_Line_14 1,1,0
+#define Basic_Line_15 1,1,1
+#define Basic_Line_16 1,1,0
+
+//Basic Line
+#define Basic_Line_Number 16
+#define Basic_Line_1 1,1,0
+#define Basic_Line_2 1,1,1
+#define Basic_Line_3 1,1,0
+#define Basic_Line_4 1,1,0
+#define Basic_Line_5 1,1,0
+#define Basic_Line_6 1,1,0
+#define Basic_Line_7 1,1,0
+#define Basic_Line_8 1,1,0
+#define Basic_Line_9 1,1,0
+#define Basic_Line_10 1,1,0
+#define Basic_Line_11 1,1,0
+#define Basic_Line_12 1,1,0
+#define Basic_Line_13 1,1,0
+#define Basic_Line_14 1,1,0
+#define Basic_Line_15 1,1,1
+#define Basic_Line_16 1,1,0
+
+//Basic Line
+#define Basic_Line_Number 16
+#define Basic_Line_1 1,1,0
+#define Basic_Line_2 1,1,1
+#define Basic_Line_3 1,1,0
+#define Basic_Line_4 1,1,0
+#define Basic_Line_5 1,1,0
+#define Basic_Line_6 1,1,0
+#define Basic_Line_7 1,1,0
+#define Basic_Line_8 1,1,0
+#define Basic_Line_9 1,1,0
+#define Basic_Line_10 1,1,0
+#define Basic_Line_11 1,1,0
+#define Basic_Line_12 1,1,0
+#define Basic_Line_13 1,1,0
+#define Basic_Line_14 1,1,0
+#define Basic_Line_15 1,1,1
+#define Basic_Line_16 1,1,0
+
+//Levels
+
+//Level One		//May be best refracted if time. 
+#define L1_Line_1 
+#define L1_Line_1 
+#define L1_Line_1 
+#define L1_Line_1 
+#define L1_Line_1 
+#define L1_Line_1 
+//Note to self add Colour blocks etc
+
+//Level Two
+#define L2_Brick_NO 64
+#define L2_Brick_Lines 6
+#define L2_Brick_Pow 4
+
+//Level Three
+#define L3_Brick_NO 64
+#define L3_Brick_Lines 6
+
+//Level Four
+#define L4_Brick_NO 64
+#define L4_Brick_Lines 6
+
+//Level Five
+#define L5_Brick_NO 64
+#define L5_Brick_Lines 6
+
+
+#endif // __LEVELDEFINE_H__
+
+
